ef42f38d3d204e6f7549a5d706dafead2f1a77d2
gabrielwp <gabrielwilliandepaula@outlook.com>
2021-02-06 16:30:38 -0300

    Adicionando IDs na tela de visualizar problema

--- scripts/viewIssue.js
+++ scripts/viewIssue.js
@@ -1,3 +1,15 @@
+const updateIssueButton = document.getElementById('update-issue-button');
+const deleteIssueButton = document.getElementById('delete-issue-button');
+const systemAnswerView = document.getElementById('system-answer');
+const answerButton = document.getElementById('answer-button');
+
+window.onload = () => {
+  updateIssueButton.classList.remove('d-none');
+  deleteIssueButton.classList.remove('d-none');
+  systemAnswerView.classList.remove('d-none');
+  answerButton.classList.remove('d-none');
+};
+
 function openDeleteIssueModal() {
   $('#delete-issue-modal').modal('toggle');
   $('#delete-issue-modal').modal('show');
